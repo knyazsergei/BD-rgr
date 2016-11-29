63c69913e3d5a31ea15b4d6c5bc1a07b244d2e99
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 11:50:33 +0300

    Добавлен класс Cregistrar

--- auth/register.php
+++ auth/register.php
@@ -1,25 +1,39 @@
 <?
 include '../config.php'; 
 
-function CheckLogin($login)
+class Cregistrar
 {
-	return (strlen($login) > 3) && preg_match("/^[a-z]+([-_]?[a-z0-9]+){0,2}$/i",$login);
+	public function Add($login)
+	{
+		if(!$this->CheckLogin($login))
+		{
+			echo $this->m_error;
+			return;
+		}
+		echo "User registered";
+	}
+
+	private function CheckLogin($login)
+	{
+		if(!(strlen($login) > 3) && preg_match("/^[a-z]+([-_]?[a-z0-9]+){0,2}$/i",$login))
+		{
+			$this->m_error = "Invalid login";
+			return false;
+		}
+		$query = mysql_query("SELECT COUNT(*) FROM users WHERE login='".mysql_real_escape_string($login)."'")or die ("<br>Invalid query: " . mysql_error()); 
+		
+		if(mysql_result($query, 0) > 0) 
+		{
+			$this->m_error = "User exists"; 
+			return false;
+		} 
+		return true;
+
+	}
+
+	private $m_error = "Unknown error";
 }
 
-$login = $_GET["login"]
-
-if(CheckLogin())
-{
-	echo "all ok";
-}
-else
-{
-	echo "login is bad";
-}
-
-$query = mysql_query("SELECT COUNT(*) FROM users WHERE login='".mysql_real_escape_string($login)."'")or die ("<br>Invalid query: " . mysql_error()); 
-if(mysql_result($query, 0) > 0) 
-{
-	echo "User exists"; 
-} 
+$registrar = new Cregistrar;
+$registrar->Add($_GET["login"]);
 ?>
