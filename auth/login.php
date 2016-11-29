a57bfbb352766c653056793aa75b67aa90ad1ed2
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 12:29:17 +0300

    Добавлена хэш функция

--- auth/login.php
+++ auth/login.php
@@ -21,9 +21,27 @@ class Cauthorizer
 			$this->m_error = "Invalid password";
 			echo $this->m_error;
 			return false;
-		} 
+		}
+
+		$hash = md5($this->CodeGenerator(10)); 
+		mysql_query("UPDATE users SET hash='".$hash."' WHERE id='".$user['id']."'") or die("MySQL Error: " . mysql_error()); 
+
+		setcookie("id", $user['id'], time()+60*60*24*30); 
+		setcookie("hash", $hash, time()+60*60*24*30); 
 
 		echo "You are logged";
+	}
+
+	private function CodeGenerator($length=6) 
+	{ 
+    	$chars = implode("",range('a', 'z'));
+    	$code = ""; 
+    	$clen = strlen($chars) - 1;   
+    	while (strlen($code) < $length) 
+    	{ 
+    	    $code .= $chars[mt_rand(0,$clen)];   
+    	} 
+    	return $code; 
 	}
 
 	private $m_error = "Unknown error";
