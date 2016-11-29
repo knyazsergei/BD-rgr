399eef4a19548e7378a33598981716e0ce33fac7
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 12:18:05 +0300

    Добавлена авторизация пользователя

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,34 @@
 <?
+include '../config.php'; 
 
+class Cauthorizer
+{
+	public function Check($login, $password)
+	{
+		$user = mysql_fetch_assoc(
+			mysql_query("SELECT id, password FROM `users` WHERE `login`='".mysql_real_escape_string($login)."'")
+		); 
 
+		if(empty($user))
+		{
+			$this->m_error = "Unknown user";
+			echo $this->m_error;
+			return false;
+		}
+
+		if(!($user['password'] === md5(md5($password))))
+		{
+			$this->m_error = "Invalid password";
+			echo $this->m_error;
+			return false;
+		} 
+
+		echo "You are logged";
+	}
+
+	private $m_error = "Unknown error";
+}
+
+$authorizer = new Cauthorizer;
+$authorizer->Check($_GET["login"], $_GET["password"]);
 ?> 
