672100ba858d4a0aaa93ec67b2209f12f58e65aa
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 11:05:05 +0300

    Добавлено подключение к базе данных

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?
 $dbhost = "localhost";
-$dbuser = "RGR";
+$dbuser = "admin";
 $dbpassword = "123456";
-$dbname = "admin";
+$dbname = "rgr";
 
-
+mysql_connect($dbhost, $dbuser, $dbpassword) or die ("MySQL Error: " . mysql_error());
+mysql_query("set names utf8") or die ("<br>Invalid query: " . mysql_error());
+mysql_select_db($dbname) or die ("<br>Invalid query: " . mysql_error());
 ?>
