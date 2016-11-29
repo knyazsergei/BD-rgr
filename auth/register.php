9b3991cdcd00e88f2ad16de8127fd2273f1842ef
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 11:25:38 +0300

    Добавлена проверка на существование пользователя в базе

--- auth/register.php
+++ auth/register.php
@@ -6,7 +6,9 @@ function CheckLogin($login)
 	return (strlen($login) > 3) && preg_match("/^[a-z]+([-_]?[a-z0-9]+){0,2}$/i",$login);
 }
 
-if(CheckLogin($_GET["login"]))
+$login = $_GET["login"]
+
+if(CheckLogin())
 {
 	echo "all ok";
 }
@@ -14,4 +16,10 @@ else
 {
 	echo "login is bad";
 }
+
+$query = mysql_query("SELECT COUNT(*) FROM users WHERE login='".mysql_real_escape_string($login)."'")or die ("<br>Invalid query: " . mysql_error()); 
+if(mysql_result($query, 0) > 0) 
+{
+	echo "User exists"; 
+} 
 ?>
