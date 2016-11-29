2bd1ce839b2c6db72d7fd14c13d2a610e6be11cf
Knyazev Sergey <yaknyazsergei@gmail.com>
2016-11-29 11:58:45 +0300

    Доблен запрос добавления пользователя в таблицу

--- auth/register.php
+++ auth/register.php
@@ -3,14 +3,18 @@ include '../config.php';
 
 class Cregistrar
 {
-	public function Add($login)
+	public function Add($login, $email, $password)
 	{
 		if(!$this->CheckLogin($login))
 		{
-			echo $this->m_error;
+			echo "error: ".$this->m_error;
 			return;
 		}
-		echo "User registered";
+		
+		$password = md5(md5(trim($password))); 
+
+		mysql_query("INSERT INTO users SET login='".$login."', email='".$email."', password='".$password."'") or die ("<br>Invalid query: " . mysql_error()); 
+		echo "Registration successful";
 	}
 
 	private function CheckLogin($login)
@@ -20,7 +24,8 @@ class Cregistrar
 			$this->m_error = "Invalid login";
 			return false;
 		}
-		$query = mysql_query("SELECT COUNT(*) FROM users WHERE login='".mysql_real_escape_string($login)."'")or die ("<br>Invalid query: " . mysql_error()); 
+
+		$query = mysql_query("SELECT COUNT(*) FROM users WHERE login='".mysql_real_escape_string($login)."'") or die ("<br>Invalid query: " . mysql_error()); 
 		
 		if(mysql_result($query, 0) > 0) 
 		{
@@ -35,5 +40,5 @@ class Cregistrar
 }
 
 $registrar = new Cregistrar;
-$registrar->Add($_GET["login"]);
+$registrar->Add($_GET["login"],$_GET["email"],$_GET["password"]);
 ?>
